5622352430fc4998d86e877f8252bc89f4a339eb
FrancescaDiMatteo <64288535+FrancescaDiMatteo@users.noreply.github.com>
2020-08-18 18:23:00 -0400

    Update university_town.py

--- university_town.py
+++ university_town.py
@@ -2,23 +2,29 @@ import pandas as pd
 import numpy as np
 from scipy.stats import ttest_ind
 
-'''The first step is to clean the university towns data. In the txt file, there is a list
-of Regions followed by all the towns, which are part of the Region.
-Each Region ends with '[edit]'.'''
+'''The first step is to clean the university towns data'''
 def univ_town():
     state = None
     state_towns = []
+    #open the university_towns file
     with open('/Users/francescadimatteo/Desktop/university_towns.txt') as file:
         for line in file:
+            #remove the space from the end line
             text = line[:-1]
+            #each state ends with [edit]
             if text[-6:] == '[edit]':
                 state = text[:-6]
                 continue
+            #some towns will have brackets, which I need to remove to have a neat table
             if '(' in line:
                 town = text[:text.index('(')-1]
                 state_towns.append([state,town])
             else:
                 town = text
                 state_towns.append([state,town])
+    #create the dataframe with the state and region, made by only neat names
     university_towns = pd.DataFrame(state_towns,columns= ['State','RegionName'])
-    return university_towns
+    print(university_towns)
+    
+    univ_town()
+    
